a0c2a8f662ba598ff428eeb819bcf53c6c40e25c
303 <303@study.local>
2022-03-03 14:19:24 +0300

    design

--- resources/views/layouts/pattern.blade.php
+++ resources/views/layouts/pattern.blade.php
@@ -6,34 +6,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel-App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/app.css">
 </head>
 
 <body class="d-flex flex-column h-100">
     <header class="d-flex justify-content-center py-3">
         <ul class="nav nav-pills">
-            <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">Главная</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">О компании</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Отзывы</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Контакты</a></li>
+            <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">Main</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Reviews</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Contacts</a></li>
+            <li class="nav-item ">
+                <button type="button" class="btn mx-2" data-bs-toggle="modal" data-bs-target="#signUpForm">
+                    Sign Up
+                </button>
+                <button type="button" class="btn mx-2" data-bs-toggle="modal" data-bs-target="#signInForm">
+                    Sign In
+                </button>
+            </li>
         </ul>
+
     </header>
+
+    {{-- modals --}}
+    <div class="modal" tabindex="-1" id="signUpForm">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Sign Up</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="" method="post">
+                        <input type="text" name="" id="" class="form-control" placeholder="User name">
+                        <input type="password" name="" id="" class="form-control" placeholder="Password">
+                        <button type="button" class="btn btn-primary">Sign Up</button>
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    {{-- modals --}}
+
     <div class="container">
 
         @yield('content')
-        <div id="root">
-
-        </div>
-
     </div>
 
-    <footer class="footer mt-auto py-3">
+    <footer class="footer mt-auto py-3 d-flex justify-content-center ">
         <span class="text-muted">&copy;Laravel-App 2022</span>
     </footer>
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
-    <script src="{{mix('js/app.js')}}"></script> --}}
+    <script src="js/app.js"></script>
 </body>
 
 </html>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,5 +2,23 @@
 
 
 @section('content')
-    
+    <section></section>
+
+    <section id="orders">
+        <div class="row">
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <div class="images">
+                        <div style="background: url('../img/img1.jpg') center center"></div>
+                        <div style="background: url('../img/bg-img.jpg') center center"></div>
+
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Order name</h5>
+                        <p class="card-text">order type</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
